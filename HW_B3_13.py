c100a1c88f1363452184c843b73f5a25d42adb1b
AlexanderPron <46918613+AlexanderPron@users.noreply.github.com>
2020-07-21 15:31:36 +0300

    Add files via upload

--- HW_B3_13.py
+++ HW_B3_13.py
@@ -1,11 +1,63 @@
+
+    # Генерируем страничку такого вида:
+    # <html>
+    # 	<head>
+    # 		<meta charset="utf-8"/>
+    # 		<title >test a4.5</title>
+    # 	</head>
+    # 	<body>
+    # 		<div class="div-0 div-1">
+    # 			<div class="div-2">
+    # 				<div class="div-3"></div>
+    # 			</div>
+    # 		</div>
+    # 		<form action="https:/google.com/search" method="GET">
+    # 			<label for="google-find">Find via Google: </label>
+    # 			<input class="simple-input" type="search" name="q" id="google-find" placeholder="Find!"/>
+    # 			<button class="simple-button bs-button" type="submit">Send</button>
+    # 		</form>
+    # 	</body>
+    # </html>
+    #================ Как пользоваться генератором HTML ===========================
+    # В начале создается основной тэг <html> при помощи менеджера контекста with, в котором определяем метод вывода результата: если output = None, то вывод происходит в консоль, иначе - в файл по пути и названию, содержащемся в переменной filePath 
+    # Затем создаем тэг верхнего уровня c отступом в 1 табуляцию (уровень вложенности = 1 - по умолчанию)
+    # Для оргнизации вложенности тегов, необходимо оргнизовать вложенность менеджеров контекста with 
+    # Затем создаем обычный тэг (см. описание класса Tag), передаем в качестве параметров название тега, уровень вложенности родителя, кортеж css классов, атрибуты тэга
+    # Если в качестве аттрибута передается зарезервированное в python слово (for, type  т.п.), то перед таким аттрибутом необходимо поставить нижнее подчеркивание _ (в конструкторе класса это подчеркивание удалим)
+    # Реализация практически полностью взята из разбора ДЗ, с небольшими дополнениями
 from TagsClasses import *
 
-def main():
-    html=""
-    with HTML(html) as doc:
+def main(output=None):
+    with HTML(output=output) as doc:
         with TopLevelTag("head") as head:
-            html += head
-        print(doc)
+            with Tag("meta", head.indentLevel, is_single=True, charset="utf-8") as meta:
+                head += meta
+            with Tag("title", head.indentLevel) as title:
+                title.text = "test a4.5"
+                head += title
+            doc += head
+        with TopLevelTag("body") as body:
+            with Tag("div", body.indentLevel, klass=("div-0","div-1",)) as div1:
+                with Tag("div", div1.indentLevel, klass=("div-2",)) as div2:
+                    with Tag("div", div2.indentLevel, klass=("div-3",)) as div3:
+                        div2 += div3
+                    div1 += div2
+                body += div1
+
+            with Tag("form", body.indentLevel, action="https:/google.com/search", method="GET") as form:
+                with Tag("label", form.indentLevel, _for = "google-find") as label: # В конструкторе Tag уберём нижнее подчеркивание _ (иначе нельзя передать параметр с зарезервированным словом for)
+                    label.text = "Find via Google: "
+                    form += label
+                with Tag("input", form.indentLevel, is_single=True, klass = ("simple-input",), _type="search", name="q", id="google-find", placeholder="Find!") as input:
+                    form += input
+                with Tag("button", form.indentLevel, klass = ("simple-button", "bs-button",), _type="submit") as button:
+                    button.text = "Send"
+                    form += button
+                body += form
+            doc += body
+
 
 if __name__ == "__main__":
-    main()
+    # filePath = None  # None - если хотим вывод в консоль
+    filePath = "index.html"
+    main(filePath)
--- TagsClasses.py
+++ TagsClasses.py
@@ -1,23 +1,109 @@
-class HTML():
-    def __init__(self, html):
-        self.html = html
-    def __enter__(self):
-        self.html = "<html>\n"
-        return self
-    def __exit__(self, type, value, traceback):
-        pass
-    def __str__(self):
-        self.html += "</html>"
-        return self.html
-class TopLevelTag(HTML):
-    def __init(self, html, tag):
+class Tag:
+    '''Класс, описывающий отображение тэгов, которые могут быть одиночными или парными, могут иметь css классы (klass), могут иметь атрибуты (**kwargs). 
+    indentLevel - принимает уровень вложенности тэга - родителя (нужно для оформления вывода с отступами)'''
+    def __init__(self, tag, indentLevel, is_single=False, klass=None, **kwargs):
         self.tag = tag
-    def __enter__(self):
-        self.html = HTML.__enter__ + "<{}>\n".format(self.tag)
+        self.text = ""
+        self.attributes = {}
+        self.is_single = is_single
+        self.children = []
+        self.indentLevel = indentLevel
+        if klass is not None:
+            self.attributes["class"] = " ".join(klass)
+
+        for attr, value in kwargs.items():
+            if "_" in attr:
+                attr = attr.replace("_", "")
+            self.attributes[attr] = value
+
+    def __enter__(self, *args, **kwargs):
+        self.indentLevel += 1       # При входе в контекст увеличиваем уровень вложенности на 1
         return self
-    def __iadd__(self):
-        self.html += "</{}}>".format(self.tag)
-        return self.html
-class Tag():
-    pass
+
+    def __exit__(self, *args, **kwargs):
+        pass
+    def __iadd__(self, other):
+        self.children.append(other)
+        return self
+
+    def __str__(self):
+        attrs = []
+        for attribute, value in self.attributes.items():
+            attrs.append('%s="%s"' % (attribute, value))
+        attrs = " ".join(attrs)
+
+        if len(self.children) > 0:
+            opening = "\n"+"\t"*self.indentLevel+"<{tag} {attrs}>".format(tag=self.tag, attrs=attrs)
+            if self.text:
+                internal = "%s" % self.text
+            else:
+                internal = ""
+            for child in self.children:
+                internal += str(child)
+            ending = "\n"+"\t"*self.indentLevel+"</%s>" % self.tag
+            return opening + internal + ending
+        else:
+            if self.is_single:
+                return "\n"+"\t"*self.indentLevel+"<{tag} {attrs}/>".format(tag=self.tag, attrs=attrs)
+            else:
+                return "\n"+"\t"*self.indentLevel+"<{tag} {attrs}>{text}".format(tag=self.tag, attrs=attrs, text=self.text)+"</{tag}>".format(tag=self.tag)
+
+
+class HTML:
+    '''Класс, описывающий вывод внешних тегов html документа <html> </html>, а также в нём задается способ вывода результата генерации html-документа:
+    если output = None, то вывод в консоль
+    если output = "путь/имя_файла", то вывод будет осуществлен в файл, содержимое файла будет перезаписано'''
+    def __init__(self, output=None):
+        self.output = output
+        self.children = []
+
+    def __iadd__(self, other):
+        self.children.append(other)
+        return self
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        if self.output is not None:
+            with open(self.output, "w") as fp:
+                fp.write(str(self))
+        else:
+            print(self)
+
+    def __str__(self):
+        html = "<html>\n"
+        for child in self.children:
+            html += str(child)
+        html += "</html>"
+        return html
+
+
+class TopLevelTag:
+    ''' Класс, описывающий отображение тегов верхнего уровня, такие как <head>, <body> и т.д. Они всегда парные, не сродержат текста внутри себя и для упрощения не содержат атрибутов. 
+    По умолчанию принимает indentLevel = 1 что соответствует первому уровню вложенности, т.к. такие теги находятся лишь внутри <html>. indentLevel введено лишь для вывода сгенерированного html документа с корректными отступами'''
+    def __init__(self, tag, indentLevel = 1, **kwargs):
+        self.tag = tag
+        self.children = []
+        self.indentLevel = indentLevel
+
+    def __iadd__(self, other):
+        self.children.append(other)
+        return self
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        pass
+
+    def __str__(self):
+        html = "\t"*self.indentLevel +"<%s>" % self.tag
+        for child in self.children:
+            html += str(child)
+        html += "\n"+"\t"*self.indentLevel + "</%s>\n" % self.tag
+        return html
+
+
+
 
